1641eb8a8ade21c79c782567e3b763f2deac8cf3
PollyPanic <181524074+PollyPanic@users.noreply.github.com>
2026-02-04 20:29:39 +0100

    Add card toggle functionality

--- src/callbacks/ui_callbacks.py
+++ src/callbacks/ui_callbacks.py
@@ -1,4 +1,4 @@
-from dash import callback, Input, Output
+from dash import callback, Input, Output, State, MATCH
 from src.index import app
 
 @callback(
@@ -14,3 +14,18 @@ def toggle_edit_room_collapse(val):
 )
 def toggle_new_room_collapse(val):
     return val == 'OTHER'
+
+@app.callback(
+    Output({'type': 'card-collapse', 'index': MATCH}, 'is_open'),
+    Output({'type': 'card-toggler', 'index': MATCH}, 'children'),
+    Input({'type': 'card-toggler', 'index': MATCH}, 'n_clicks'),
+    State({'type': 'card-collapse', 'index': MATCH}, 'is_open'),
+    prevent_initial_call=True
+)
+def toggle_card(n, is_open):
+    if n:
+        if is_open:
+            return False, "Details"
+        else:
+            return True, "Schließen"
+    return is_open, "Details"
